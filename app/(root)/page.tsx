2db1255b593e0073036d8c7001ef4e9efbcfcd55
nima.yaghouti <nima.yaghouti99@gmail.com>
2025-11-22 18:47:55 +0330

    feat: add StartupCard component and integrate with Home page for displaying startup listings

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,8 @@
+import { client } from '@/sanity/lib/client';
+import { STARTUP_QUERY } from '@/sanity/lib/queries';
+
 import SearchForm from '@/components/SearchForm';
+import StartupCard, { StartupCardType } from '@/components/StartupCard';
 
 export default async function Home({
   searchParams,
@@ -6,6 +10,8 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = await client.fetch(STARTUP_QUERY);
 
   return (
     <>
@@ -20,6 +26,22 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p className="no-result">No startups found.</p>
+          )}
+        </ul>
       </section>
     </>
   );
